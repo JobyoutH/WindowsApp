7b3cd1e0baff49fcf893c8ddf2e5659962288024
Joby_out H <ztytotoro@outlook.com>
2015-09-20 23:30:22 +0800

    Almost Done

---
+++ Triangle area cal/Triangle area cal/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Triangle_area_cal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ina1 = new System.Windows.Forms.TextBox();
+            this.inb1 = new System.Windows.Forms.TextBox();
+            this.inc1 = new System.Windows.Forms.TextBox();
+            this.out1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // ina1
+            // 
+            this.ina1.Location = new System.Drawing.Point(384, 49);
+            this.ina1.MaxLength = 8;
+            this.ina1.Name = "ina1";
+            this.ina1.Size = new System.Drawing.Size(100, 21);
+            this.ina1.TabIndex = 0;
+            this.ina1.TextChanged += new System.EventHandler(this.sTextChanged);
+            this.ina1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.num_KeyPress);
+            // 
+            // inb1
+            // 
+            this.inb1.Location = new System.Drawing.Point(517, 49);
+            this.inb1.MaxLength = 8;
+            this.inb1.Name = "inb1";
+            this.inb1.Size = new System.Drawing.Size(100, 21);
+            this.inb1.TabIndex = 1;
+            this.inb1.TextChanged += new System.EventHandler(this.sTextChanged);
+            // 
+            // inc1
+            // 
+            this.inc1.Location = new System.Drawing.Point(659, 49);
+            this.inc1.MaxLength = 8;
+            this.inc1.Name = "inc1";
+            this.inc1.Size = new System.Drawing.Size(100, 21);
+            this.inc1.TabIndex = 2;
+            this.inc1.TextChanged += new System.EventHandler(this.sTextChanged);
+            // 
+            // out1
+            // 
+            this.out1.Location = new System.Drawing.Point(789, 49);
+            this.out1.MaxLength = 11;
+            this.out1.Name = "out1";
+            this.out1.Size = new System.Drawing.Size(100, 21);
+            this.out1.TabIndex = 3;
+            this.out1.Visible = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(16, 27);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(353, 436);
+            this.panel1.TabIndex = 4;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(901, 497);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.out1);
+            this.Controls.Add(this.inc1);
+            this.Controls.Add(this.inb1);
+            this.Controls.Add(this.ina1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox ina1;
+        private System.Windows.Forms.TextBox inb1;
+        private System.Windows.Forms.TextBox inc1;
+        private System.Windows.Forms.TextBox out1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Triangle area cal/Triangle area cal/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Triangle_area_cal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();            
+        }
+
+        private double AeraCalculator(double a, double b, double c)
+        {
+            double s, p = (a + b + c) / 2;
+
+            if (a + b > c && a + c > b && b + c > a)
+            {
+                return s = System.Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            }
+            else
+            {
+                return s = 0;
+            }
+        }
+
+        private void num_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!((e.KeyChar >= '0' && e.KeyChar <= '9') || e.KeyChar == '.' || e.KeyChar == ''))
+            {
+                e.Handled = true;
+            }
+            else
+            {
+                if (e.KeyChar == '.')
+                {
+                    string str = this.Text;
+                    int num = str.IndexOf('.');
+                    if (num != -1)
+                    {
+                        e.Handled = true;
+                    }
+                }
+            }
+        }
+
+        private void sTextChanged(object sender, EventArgs e)
+        {
+            if (ina1.Text != null && inb1.Text != null && inc1.Text != null)
+            {
+                double a, b, c, s;
+
+                try
+                {
+                    a = Double.Parse(ina1.Text);
+                    b = Double.Parse(inb1.Text);
+                    c = Double.Parse(inc1.Text);
+
+                    s = AeraCalculator(a, b, c);
+
+                    if (s == 0)
+                    {
+                        out1.Text = "null";
+                    }
+                    else
+                    {
+                        out1.Text = s.ToString("0.00");
+                        panel1.Invalidate();
+                    }  
+
+                    out1.Show();
+
+                }
+                catch (FormatException)
+                { }
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics gc = e.Graphics;
+
+            float a, b, c, x, y, a1, b1, c1, x1, y1, x2, temp;
+            try
+            {
+                a1 = float.Parse(ina1.Text);
+                b1 = float.Parse(inb1.Text);
+                c1 = float.Parse(inc1.Text);
+
+                if (a1 < b1)
+                {
+                    temp = a1;
+                    a1 = b1;
+                    b1 = temp;
+                }
+
+                if (a1 < c1)
+                {
+                    temp = a1;
+                    a1 = c1;
+                    c1 = temp;
+                }
+
+                a = 250;
+                b = a / a1 * b1;
+                c = a / a1 * c1;
+
+
+                x1 = (2 * panel1.Left + panel1.Width - a) / 2;
+                x2 = (2 * panel1.Left + panel1.Width + a) / 2;
+
+                x = (x1 + x2 + (b * b - c * c) / (x2 - x1)) / 2;
+                y1 = panel1.Top + panel1.Height / 2 - (float)Math.Sqrt(b * b - (x - x1) * (x - x1)) / 3;
+                y = (float)(3 * panel1.Top + 1.5 * panel1.Height - 2 * y1);
+
+                Pen pen = new Pen(Color.Black, 1);
+                gc.DrawLine(pen, x1, y1, x2, y1);
+                gc.DrawLine(pen, x2, y1, x, y);
+                gc.DrawLine(pen, x, y, x1, y1);
+            }
+            catch (FormatException)
+            { }
+            
+        }
+    }
+}
