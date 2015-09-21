8043324d5f479d7de74f7f4249f3ef29a842c7dc
Joby_out H <ztytotoro@outlook.com>
2015-09-21 12:39:20 +0800

    三角形显示修正

--- Triangle area cal/Triangle area cal/Form1.Designer.cs
+++ Triangle area cal/Triangle area cal/Form1.Designer.cs
@@ -33,64 +33,140 @@
             this.inc1 = new System.Windows.Forms.TextBox();
             this.out1 = new System.Windows.Forms.TextBox();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.Data = new System.Windows.Forms.GroupBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.A = new System.Windows.Forms.TextBox();
+            this.B = new System.Windows.Forms.TextBox();
+            this.S = new System.Windows.Forms.TextBox();
+            this.C = new System.Windows.Forms.TextBox();
+            this.Data.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // ina1
             // 
-            this.ina1.Location = new System.Drawing.Point(384, 49);
+            this.ina1.Location = new System.Drawing.Point(6, 9);
             this.ina1.MaxLength = 8;
             this.ina1.Name = "ina1";
-            this.ina1.Size = new System.Drawing.Size(100, 21);
+            this.ina1.Size = new System.Drawing.Size(100, 23);
             this.ina1.TabIndex = 0;
             this.ina1.TextChanged += new System.EventHandler(this.sTextChanged);
             this.ina1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.num_KeyPress);
             // 
             // inb1
             // 
-            this.inb1.Location = new System.Drawing.Point(517, 49);
+            this.inb1.Location = new System.Drawing.Point(138, 9);
             this.inb1.MaxLength = 8;
             this.inb1.Name = "inb1";
-            this.inb1.Size = new System.Drawing.Size(100, 21);
+            this.inb1.Size = new System.Drawing.Size(100, 23);
             this.inb1.TabIndex = 1;
             this.inb1.TextChanged += new System.EventHandler(this.sTextChanged);
             // 
             // inc1
             // 
-            this.inc1.Location = new System.Drawing.Point(659, 49);
+            this.inc1.Location = new System.Drawing.Point(268, 9);
             this.inc1.MaxLength = 8;
             this.inc1.Name = "inc1";
-            this.inc1.Size = new System.Drawing.Size(100, 21);
+            this.inc1.Size = new System.Drawing.Size(100, 23);
             this.inc1.TabIndex = 2;
             this.inc1.TextChanged += new System.EventHandler(this.sTextChanged);
             // 
             // out1
             // 
-            this.out1.Location = new System.Drawing.Point(789, 49);
+            this.out1.Enabled = false;
+            this.out1.Location = new System.Drawing.Point(395, 9);
             this.out1.MaxLength = 11;
             this.out1.Name = "out1";
-            this.out1.Size = new System.Drawing.Size(100, 21);
+            this.out1.Size = new System.Drawing.Size(100, 23);
             this.out1.TabIndex = 3;
-            this.out1.Visible = false;
             // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(16, 27);
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel1.Location = new System.Drawing.Point(12, 37);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(353, 436);
+            this.panel1.Size = new System.Drawing.Size(353, 415);
             this.panel1.TabIndex = 4;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // Data
+            // 
+            this.Data.Controls.Add(this.ina1);
+            this.Data.Controls.Add(this.inb1);
+            this.Data.Controls.Add(this.out1);
+            this.Data.Controls.Add(this.inc1);
+            this.Data.Location = new System.Drawing.Point(394, 90);
+            this.Data.Name = "Data";
+            this.Data.Size = new System.Drawing.Size(495, 34);
+            this.Data.TabIndex = 5;
+            this.Data.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.A);
+            this.groupBox1.Controls.Add(this.B);
+            this.groupBox1.Controls.Add(this.S);
+            this.groupBox1.Controls.Add(this.C);
+            this.groupBox1.Location = new System.Drawing.Point(394, 50);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(495, 34);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            // 
+            // A
+            // 
+            this.A.Enabled = false;
+            this.A.Location = new System.Drawing.Point(6, 9);
+            this.A.MaxLength = 8;
+            this.A.Name = "A";
+            this.A.Size = new System.Drawing.Size(100, 23);
+            this.A.TabIndex = 0;
+            this.A.Text = "A";
+            // 
+            // B
+            // 
+            this.B.Enabled = false;
+            this.B.Location = new System.Drawing.Point(138, 9);
+            this.B.MaxLength = 8;
+            this.B.Name = "B";
+            this.B.Size = new System.Drawing.Size(100, 23);
+            this.B.TabIndex = 1;
+            this.B.Text = "B";
+            // 
+            // S
+            // 
+            this.S.Enabled = false;
+            this.S.Location = new System.Drawing.Point(395, 9);
+            this.S.MaxLength = 11;
+            this.S.Name = "S";
+            this.S.Size = new System.Drawing.Size(100, 23);
+            this.S.TabIndex = 3;
+            this.S.Text = "S";
+            // 
+            // C
+            // 
+            this.C.Enabled = false;
+            this.C.Location = new System.Drawing.Point(268, 9);
+            this.C.MaxLength = 8;
+            this.C.Name = "C";
+            this.C.Size = new System.Drawing.Size(100, 23);
+            this.C.TabIndex = 2;
+            this.C.Text = "C";
             // 
             // Form1
             // 
             this.ClientSize = new System.Drawing.Size(901, 497);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.Data);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.out1);
-            this.Controls.Add(this.inc1);
-            this.Controls.Add(this.inb1);
-            this.Controls.Add(this.ina1);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.Name = "Form1";
+            this.Text = "三角形面积计算器";
+            this.Data.ResumeLayout(false);
+            this.Data.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -102,6 +178,12 @@
         private System.Windows.Forms.TextBox inc1;
         private System.Windows.Forms.TextBox out1;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox Data;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox A;
+        private System.Windows.Forms.TextBox B;
+        private System.Windows.Forms.TextBox S;
+        private System.Windows.Forms.TextBox C;
     }
 }
 
--- Triangle area cal/Triangle area cal/Form1.cs
+++ Triangle area cal/Triangle area cal/Form1.cs
@@ -75,8 +75,6 @@ namespace Triangle_area_cal
                         panel1.Invalidate();
                     }  
 
-                    out1.Show();
-
                 }
                 catch (FormatException)
                 { }
@@ -87,18 +85,24 @@ namespace Triangle_area_cal
         {
             Graphics gc = e.Graphics;
 
-            float a, b, c, x, y, a1, b1, c1, x1, y1, x2, temp;
+            double a, b, c, x, y, a1, b1, c1, x1, y1, x2, temp;
+
+            String A = "A", B = "B", C = "C", T;
             try
             {
-                a1 = float.Parse(ina1.Text);
-                b1 = float.Parse(inb1.Text);
-                c1 = float.Parse(inc1.Text);
+                a1 = double.Parse(ina1.Text);
+                b1 = double.Parse(inb1.Text);
+                c1 = double.Parse(inc1.Text);
 
                 if (a1 < b1)
                 {
                     temp = a1;
                     a1 = b1;
                     b1 = temp;
+                    T = A.Clone().ToString();
+                    A = B.Clone().ToString();
+                    B = T.Clone().ToString();
+
                 }
 
                 if (a1 < c1)
@@ -106,24 +110,37 @@ namespace Triangle_area_cal
                     temp = a1;
                     a1 = c1;
                     c1 = temp;
+                    T = A.Clone().ToString();
+                    A = C.Clone().ToString();
+                    C = T.Clone().ToString();
                 }
 
-                a = 250;
+                a = 250.0;
                 b = a / a1 * b1;
                 c = a / a1 * c1;
 
 
-                x1 = (2 * panel1.Left + panel1.Width - a) / 2;
-                x2 = (2 * panel1.Left + panel1.Width + a) / 2;
+                x1 = (panel1.Width - a) / 2.0;
+                x2 = (panel1.Width + a) / 2.0;
 
-                x = (x1 + x2 + (b * b - c * c) / (x2 - x1)) / 2;
-                y1 = panel1.Top + panel1.Height / 2 - (float)Math.Sqrt(b * b - (x - x1) * (x - x1)) / 3;
-                y = (float)(3 * panel1.Top + 1.5 * panel1.Height - 2 * y1);
+                x = (x1 + x2 + (b * b - c * c) / (x2 - x1)) / 2.0;
+                y1 = panel1.Height / 2 + Math.Sqrt(b * b - (x - x1) * (x - x1)) / 2.0;
+                y = panel1.Height - y1;
 
                 Pen pen = new Pen(Color.Black, 1);
-                gc.DrawLine(pen, x1, y1, x2, y1);
-                gc.DrawLine(pen, x2, y1, x, y);
-                gc.DrawLine(pen, x, y, x1, y1);
+                gc.DrawLine(pen, (float)x1, (float)y1, (float)x2, (float)y1);
+                gc.DrawLine(pen, (float)x2, (float)y1, (float)x, (float)y);
+                gc.DrawLine(pen, (float)x, (float)y, (float)x1, (float)y1);
+
+                Font ff = new Font("微软雅黑", 16, FontStyle.Regular);
+                Color col = new Color();
+                col = Color.Black;
+
+                Brush bru = new SolidBrush(col);
+
+                gc.DrawString(A, ff, bru, (float)(panel1.Width / 2.0 - 8), (float)y1 + 10);
+                gc.DrawString(B, ff, bru, (float)((x1 + x) / 2.0 - 40), (float)((y1 + y) / 2.0 - 18));
+                gc.DrawString(C, ff, bru, (float)((x2 + x) / 2.0 + 20), (float)((y1 + y) / 2.0 - 18));
             }
             catch (FormatException)
             { }
