45548f4cc696a17fd797e6fa4af3ec7f5fb724c2
Joby_out H <ztytotoro@outlook.com>
2015-09-23 19:38:41 +0800

    随便改改

--- Triangle area cal/Triangle area cal/Form1.Designer.cs
+++ Triangle area cal/Triangle area cal/Form1.Designer.cs
@@ -28,24 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.ina1 = new System.Windows.Forms.TextBox();
             this.inb1 = new System.Windows.Forms.TextBox();
             this.inc1 = new System.Windows.Forms.TextBox();
             this.out1 = new System.Windows.Forms.TextBox();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.Data = new System.Windows.Forms.GroupBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Data = new System.Windows.Forms.GroupBox();
             this.A = new System.Windows.Forms.TextBox();
             this.B = new System.Windows.Forms.TextBox();
             this.S = new System.Windows.Forms.TextBox();
             this.C = new System.Windows.Forms.TextBox();
-            this.Data.SuspendLayout();
             this.groupBox1.SuspendLayout();
+            this.Data.SuspendLayout();
             this.SuspendLayout();
             // 
             // ina1
             // 
-            this.ina1.Location = new System.Drawing.Point(6, 9);
+            this.ina1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ina1.Location = new System.Drawing.Point(0, 9);
             this.ina1.MaxLength = 8;
             this.ina1.Name = "ina1";
             this.ina1.Size = new System.Drawing.Size(100, 23);
@@ -55,6 +57,7 @@
             // 
             // inb1
             // 
+            this.inb1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.inb1.Location = new System.Drawing.Point(138, 9);
             this.inb1.MaxLength = 8;
             this.inb1.Name = "inb1";
@@ -64,6 +67,7 @@
             // 
             // inc1
             // 
+            this.inc1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.inc1.Location = new System.Drawing.Point(268, 9);
             this.inc1.MaxLength = 8;
             this.inc1.Name = "inc1";
@@ -74,6 +78,7 @@
             // out1
             // 
             this.out1.Enabled = false;
+            this.out1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.out1.Location = new System.Drawing.Point(395, 9);
             this.out1.MaxLength = 11;
             this.out1.Name = "out1";
@@ -82,60 +87,65 @@
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.panel1.Location = new System.Drawing.Point(12, 37);
+            this.panel1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(65, 152);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(353, 415);
+            this.panel1.Size = new System.Drawing.Size(495, 451);
             this.panel1.TabIndex = 4;
+            this.panel1.Tag = "";
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
-            // 
-            // Data
-            // 
-            this.Data.Controls.Add(this.ina1);
-            this.Data.Controls.Add(this.inb1);
-            this.Data.Controls.Add(this.out1);
-            this.Data.Controls.Add(this.inc1);
-            this.Data.Location = new System.Drawing.Point(394, 90);
-            this.Data.Name = "Data";
-            this.Data.Size = new System.Drawing.Size(495, 34);
-            this.Data.TabIndex = 5;
-            this.Data.TabStop = false;
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.A);
-            this.groupBox1.Controls.Add(this.B);
-            this.groupBox1.Controls.Add(this.S);
-            this.groupBox1.Controls.Add(this.C);
-            this.groupBox1.Location = new System.Drawing.Point(394, 50);
+            this.groupBox1.Controls.Add(this.ina1);
+            this.groupBox1.Controls.Add(this.inb1);
+            this.groupBox1.Controls.Add(this.out1);
+            this.groupBox1.Controls.Add(this.inc1);
+            this.groupBox1.Location = new System.Drawing.Point(65, 83);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(495, 34);
-            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabIndex = 5;
             this.groupBox1.TabStop = false;
+            // 
+            // Data
+            // 
+            this.Data.Controls.Add(this.A);
+            this.Data.Controls.Add(this.B);
+            this.Data.Controls.Add(this.S);
+            this.Data.Controls.Add(this.C);
+            this.Data.Location = new System.Drawing.Point(65, 28);
+            this.Data.Name = "Data";
+            this.Data.Size = new System.Drawing.Size(495, 34);
+            this.Data.TabIndex = 6;
+            this.Data.TabStop = false;
             // 
             // A
             // 
             this.A.Enabled = false;
-            this.A.Location = new System.Drawing.Point(6, 9);
+            this.A.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A.Location = new System.Drawing.Point(0, 9);
             this.A.MaxLength = 8;
             this.A.Name = "A";
             this.A.Size = new System.Drawing.Size(100, 23);
             this.A.TabIndex = 0;
-            this.A.Text = "A";
+            this.A.Text = "a";
             // 
             // B
             // 
             this.B.Enabled = false;
+            this.B.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.B.Location = new System.Drawing.Point(138, 9);
             this.B.MaxLength = 8;
             this.B.Name = "B";
             this.B.Size = new System.Drawing.Size(100, 23);
             this.B.TabIndex = 1;
-            this.B.Text = "B";
+            this.B.Text = "b";
             // 
             // S
             // 
             this.S.Enabled = false;
+            this.S.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.S.Location = new System.Drawing.Point(395, 9);
             this.S.MaxLength = 11;
             this.S.Name = "S";
@@ -146,40 +156,45 @@
             // C
             // 
             this.C.Enabled = false;
+            this.C.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.C.Location = new System.Drawing.Point(268, 9);
             this.C.MaxLength = 8;
             this.C.Name = "C";
             this.C.Size = new System.Drawing.Size(100, 23);
             this.C.TabIndex = 2;
-            this.C.Text = "C";
+            this.C.Text = "c";
             // 
             // Form1
             // 
-            this.ClientSize = new System.Drawing.Size(901, 497);
-            this.Controls.Add(this.groupBox1);
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(627, 642);
             this.Controls.Add(this.Data);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.panel1);
-            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Font = new System.Drawing.Font("Segoe UI", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "三角形面积计算器";
-            this.Data.ResumeLayout(false);
-            this.Data.PerformLayout();
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Triangle Area Calculator";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.Data.ResumeLayout(false);
+            this.Data.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+
         private System.Windows.Forms.TextBox ina1;
         private System.Windows.Forms.TextBox inb1;
         private System.Windows.Forms.TextBox inc1;
         private System.Windows.Forms.TextBox out1;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.GroupBox Data;
         private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox Data;
         private System.Windows.Forms.TextBox A;
         private System.Windows.Forms.TextBox B;
         private System.Windows.Forms.TextBox S;
--- Triangle area cal/Triangle area cal/Form1.cs
+++ Triangle area cal/Triangle area cal/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,7 +17,7 @@ namespace Triangle_area_cal
         {
             InitializeComponent();            
         }
-
+        /*计算三角形面积，若三条边不能构成三角形，使面积等于0*/
         private double AeraCalculator(double a, double b, double c)
         {
             double s, p = (a + b + c) / 2;
@@ -31,6 +32,7 @@ namespace Triangle_area_cal
             }
         }
 
+        /*除了数字和小数点外的值都不能被输入*/
         private void num_KeyPress(object sender, KeyPressEventArgs e)
         {
             if (!((e.KeyChar >= '0' && e.KeyChar <= '9') || e.KeyChar == '.' || e.KeyChar == ''))
@@ -68,7 +70,7 @@ namespace Triangle_area_cal
 
                     s = AeraCalculator(a, b, c);
 
-                    if (s == 0)
+                    if (s == 0)             //当不能构成三角形时，弹出错误提示
                     {
                         out1.Text = "";
                         f++;
@@ -78,20 +80,20 @@ namespace Triangle_area_cal
                             case 1: MessageBox.Show("你输的是三角形吗！？"); break;
                             case 2: MessageBox.Show("Are you serious ?"); break;
                             case 3: MessageBox.Show("数体教？"); break;
-                            case 4: MessageBox.Show("数死早？"); break;
-                            case 5: MessageBox.Show("你还是回家种地吧！"); break;
-                            case 6: MessageBox.Show("你走！"); break;
-                            case 7: MessageBox.Show("爱过！"); break;
+                            case 4: MessageBox.Show("你还是回家种地吧？"); break;
+                            case 5: MessageBox.Show("有杀气？"); break;
+                            case 6: MessageBox.Show("给你3秒从这里消失！"); break;
+                            case 7: MessageBox.Show("弓箭手准备！！"); break;
                             default:
-                                MessageBox.Show("再不走打你了！"); return;
+                                MessageBox.Show("放箭！"); return;
                         }
 
                     }
                     else
                     {
-                        out1.Text = s.ToString("0.00");
+                        out1.Text = s.ToString("0.00");         //面积保留两位小数
                         f = 0;
-                        panel1.Invalidate();
+                        panel1.Invalidate();                    //每修改一次 panel1 重绘一次
                     }  
 
                 }
@@ -103,10 +105,14 @@ namespace Triangle_area_cal
         private void panel1_Paint(object sender, PaintEventArgs e)
         {
             Graphics gc = e.Graphics;
+            gc.SmoothingMode = SmoothingMode.HighQuality;  //图片柔顺模式选择
+            gc.InterpolationMode = InterpolationMode.HighQualityBicubic;//高质量
+            gc.CompositingQuality = CompositingQuality.HighQuality;//再加一点
+
 
             double a, b, c, x, y, a1, b1, c1, x1, y1, x2, temp;
 
-            String A = "A", B = "B", C = "C", T;
+            String A = "a", B = "b", C = "c", T;
             try
             {
                 a1 = double.Parse(ina1.Text);
@@ -133,7 +139,7 @@ namespace Triangle_area_cal
                     A = C.Clone().ToString();
                     C = T.Clone().ToString();
                 }
-
+                /*根据边长求出三点坐标，其中最长边固定长度，横坐标固定，并且三角形重心为panel的中心点*/
                 a = 250.0;
                 b = a / a1 * b1;
                 c = a / a1 * c1;
@@ -146,17 +152,19 @@ namespace Triangle_area_cal
                 y1 = panel1.Height / 2 + Math.Sqrt(b * b - (x - x1) * (x - x1)) / 2.0;
                 y = panel1.Height - y1;
 
+                /*根据三个点绘制三角形*/
                 Pen pen = new Pen(Color.Black, 1);
                 gc.DrawLine(pen, (float)x1, (float)y1, (float)x2, (float)y1);
                 gc.DrawLine(pen, (float)x2, (float)y1, (float)x, (float)y);
                 gc.DrawLine(pen, (float)x, (float)y, (float)x1, (float)y1);
 
-                Font ff = new Font("微软雅黑", 16, FontStyle.Regular);
+                Font ff = new Font("Segoe UI", 16, FontStyle.Regular);
                 Color col = new Color();
                 col = Color.Black;
 
                 Brush bru = new SolidBrush(col);
 
+                /*在三条边的旁边标明a b c*/
                 gc.DrawString(A, ff, bru, (float)(panel1.Width / 2.0 - 8), (float)y1 + 10);
                 gc.DrawString(B, ff, bru, (float)((x1 + x) / 2.0 - 40), (float)((y1 + y) / 2.0 - 18));
                 gc.DrawString(C, ff, bru, (float)((x2 + x) / 2.0 + 20), (float)((y1 + y) / 2.0 - 18));
